a98fa2ca25dc96644a5f781126742b2a1c59784a
Karen Hanna <karen@laveryhanna.com>
2024-07-02 16:32:04 +0100

    initial select request page added

---
+++ src/SFA.DAS.RequestApprenticeTraining.Api.UnitTests/Controllers/EmployerRequest/WhenGettingSelectEmployerRequests.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RequestApprenticeTraining.Api.Controllers;
+using SFA.DAS.RequestApprenticeTraining.Application.Queries.GetSelectEmployerRequests;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RequestApprenticeTraining.Api.UnitTests.Controllers.AggregatedEmployerRequest
+{
+    public class WhenGettingSelectEmployerRequests
+    {
+        [Test, MoqAutoData]
+        public async Task And_MediatorCommandIsSuccessful_Then_ReturnOk
+            (long accountId,
+            [Frozen] Mock<IMediator> mediator,
+            GetSelectEmployerRequestsQueryResult selectEmployerRequestResult,
+            [Greedy] EmployerRequestController controller)
+        {
+            // Arrange
+            mediator
+                .Setup(m => m.Send(It.IsAny<GetSelectEmployerRequestsQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(selectEmployerRequestResult);
+
+            // Act
+            var result = await controller.GetSelectEmployerRequests("ST0004", "1234560");
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>().Which.Value.Should().BeEquivalentTo(selectEmployerRequestResult.SelectEmployerRequests);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_MediatorCommandIsUnsuccessful_Then_ReturnBadRequest
+            (long accountId,
+            [Frozen] Mock<IMediator> mediator,
+            [Greedy] EmployerRequestController controller)
+        {
+            // Arrange
+            mediator
+                .Setup(m => m.Send(It.IsAny<GetSelectEmployerRequestsQuery>(), It.IsAny<CancellationToken>()))
+                .Throws(new Exception());
+
+            // Act
+            var result = await controller.GetSelectEmployerRequests("ST0004", "1234560");
+
+            // Assert
+            result.Should().BeOfType<BadRequestResult>();
+        }
+    }
+}
---
+++ src/SFA.DAS.RequestApprenticeTraining.Domain.UnitTests/Models/Mapping/WhenMappingFromSelectEmployerRequestEntityToModel.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.RequestApprenticeTraining.Domain.Entities;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.RequestApprenticeTraining.Domain.UnitTests.Models
+{
+    public class WhenMappingFromSelectEmployerRequestEntityToModel
+    {
+        [Test, RecursiveMoqAutoData]
+        public void ThenTheFieldsAreCorrectlyMapped(SelectEmployerRequest source)
+        {
+            var result = (Domain.Models.SelectEmployerRequest)source;
+
+            result.EmployerRequestId.Should().Be(source.EmployerRequestId);
+            result.StandardReference.Should().Be(source.StandardReference);
+            result.StandardTitle.Should().Be(source.StandardTitle);
+            result.StandardLevel.Should().Be(source.StandardLevel);
+            result.SingleLocation.Should().Be(source.SingleLocation);
+            result.DateOfRequest.Should().Be(source.DateOfRequest);
+            result.NumberOfApprentices.Should().Be(source.NumberOfApprentices);
+            result.DayRelease.Should().Be(source.DayRelease);
+            result.BlockRelease.Should().Be(source.BlockRelease);
+            result.AtApprenticesWorkplace.Should().Be(source.AtApprenticesWorkplace);
+            result.IsNew.Should().Be(source.IsNew);
+            result.IsContacted.Should().Be(source.IsContacted);
+            result.Locations.Should().BeEquivalentTo(source.Locations);
+        }
+    }
+}
