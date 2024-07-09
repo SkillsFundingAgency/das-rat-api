96fbe0e92bb67b1ad8c003d486f02e3429dc49f2
Chris Woodcock <chris@humesoftware.com>
2024-07-09 13:01:26 +0100

    Explicitly defined history tables

---
+++ src/SFA.DAS.RequestApprenticeTraining.Database/Tables/RegionHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[RegionHistory](
+    [Id] INT NOT NULL,
+    [SubregionName] VARCHAR(250) NULL,
+    [RegionName] VARCHAR(25) NULL,
+    [Latitude] FLOAT NOT NULL,
+    [Longitude] FLOAT NOT NULL,
+    [ValidFrom] DATETIME2 (0) NOT NULL,
+    [ValidTo] DATETIME2 (0) NOT NULL,
+)
+GO
