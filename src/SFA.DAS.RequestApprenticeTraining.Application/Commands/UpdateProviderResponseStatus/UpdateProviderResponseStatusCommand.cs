65f9705be0204f183fd2afb33ba8c1dd7cf73683
Karen Hanna <karen@laveryhanna.com>
2024-07-26 14:31:10 +0100

    Added new table for marking EmployerRequests as Viewed by Providers

---
+++ src/SFA.DAS.RequestApprenticeTraining.Application/Commands/UpdateProviderResponseStatus/UpdateProviderResponseStatusCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.RequestApprenticeTraining.Application.Commands.UpdateProviderResponseStatus
+
+{
+    public class UpdateProviderResponseStatusCommand : IRequest<UpdateProviderResponseStatusCommandResponse>
+    {
+        public long Ukprn { get; set; }
+        public List<Guid> EmployerRequestIds { get; set; }
+        public int ProviderResponseStatus { get; set; }
+    }
+}
