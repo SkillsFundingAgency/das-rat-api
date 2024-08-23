b7c5ed487ed28e8caef51503d2356c99433bc3c3
Chris Woodcock <chris@humesoftware.com>
2024-08-23 17:21:44 +0100

    Corrected whitespace differences

--- src/SFA.DAS.RequestApprenticeTraining.Database/Tables/Standard.sql
+++ src/SFA.DAS.RequestApprenticeTraining.Database/Tables/Standard.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Standard]
 (
-	[StandardReference] VARCHAR(25) NOT NULL PRIMARY KEY, 
+    [StandardReference] VARCHAR(25) NOT NULL PRIMARY KEY, 
     [StandardTitle] VARCHAR(100) NOT NULL,
     [StandardLevel] INT NOT NULL,
     [StandardSector] VARCHAR(100) NOT NULL
