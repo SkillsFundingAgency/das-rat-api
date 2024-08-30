183fe0b0eb91b284af0c2d4bc83d2009ef00a46f
Chris Woodcock <chris@humesoftware.com>
2024-08-30 15:42:58 +0100

    Removed redundant settings

--- src/SFA.DAS.RequestApprenticeTraining.Application/Queries/GetSettings/GetSettingsQueryResult.cs
+++ src/SFA.DAS.RequestApprenticeTraining.Application/Queries/GetSettings/GetSettingsQueryResult.cs
@@ -5,7 +5,5 @@
         public int ExpiryAfterMonths { get; set; }
         public int EmployerRemovedAfterExpiryMonths { get; set; }
         public int ProviderRemovedAfterRequestedMonths { get; set; }
-        public int ProviderRemovedAfterExpiryNotRespondedMonths { get; set; }
-        public int ProviderRemovedAfterExpiryRespondedMonths { get; set; }
     }
 }
