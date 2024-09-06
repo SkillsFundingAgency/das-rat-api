86746bb67b389cddf2321bbd91c75063e7b10dc8
Karen Hanna <karen@laveryhanna.com>
2024-09-06 17:06:14 +0100

    added unit tests

---
+++ src/SFA.DAS.RequestApprenticeTraining.Domain.UnitTests/Models/Mapping/WhenMappingFromEmployerRequestForResponseNotificationEntityToModel.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.RequestApprenticeTraining.Domain.Entities;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.RequestApprenticeTraining.Domain.UnitTests.Models
+{
+    public class WhenMappingFromEmployerRequestForResponseNotificationEntityToModel
+    {
+        [Test, RecursiveMoqAutoData]
+        public void ThenTheFieldsAreCorrectlyMapped(EmployerRequestForResponseNotification source)
+        {
+            var result = (Domain.Models.EmployerRequestForResponseNotification)source;
+
+            result.AccountId.Should().Be(source.AccountId);
+            result.RequestedBy.Should().Be(source.RequestedBy);
+            result.Standards.Should().BeEquivalentTo(source.Standard);
+        }
+    }
+}
