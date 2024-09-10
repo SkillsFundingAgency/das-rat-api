a6500d93e541fbeb879f25fb1ebaa040953800c2
Chris Woodcock <chris@humesoftware.com>
2024-09-10 15:37:56 +0100

    Added cancel request endpoint and command

--- src/SFA.DAS.RequestApprenticeTraining.Application/Commands/AcknowledgeProviderResponses/AcknowledgeProviderResponsesCommandHandler.cs
+++ src/SFA.DAS.RequestApprenticeTraining.Application/Commands/AcknowledgeProviderResponses/AcknowledgeProviderResponsesCommandHandler.cs
@@ -4,19 +4,23 @@ using SFA.DAS.RequestApprenticeTraining.Domain.Interfaces;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using static SFA.DAS.RequestApprenticeTraining.Domain.Models.Enums;
 
 namespace SFA.DAS.RequestApprenticeTraining.Application.Commands.AcknowledgeProviderResponses
 {
     public class AcknowledgeProviderResponsesCommandHandler : IRequestHandler<AcknowledgeProviderResponsesCommand>
     {
         private readonly IEmployerRequestEntityContext _employerRequestEntityContext;
+        private readonly IDateTimeProvider _dateTimeProvider;
         private readonly ILogger<AcknowledgeProviderResponsesCommandHandler> _logger;
 
         public AcknowledgeProviderResponsesCommandHandler(
             IEmployerRequestEntityContext employerRequestEntityContext,
+            IDateTimeProvider dateTimeProvider,
             ILogger<AcknowledgeProviderResponsesCommandHandler> logger)
         {
             _employerRequestEntityContext = employerRequestEntityContext;
+            _dateTimeProvider = dateTimeProvider;
             _logger = logger;
         }
 
@@ -25,11 +29,11 @@ namespace SFA.DAS.RequestApprenticeTraining.Application.Commands.AcknowledgeProv
             _logger.LogDebug("Acknowledging provider responses for EmployerRequestId: {EmployerRequestId}", request.EmployerRequestId);
 
             var employerRequest = await _employerRequestEntityContext.Get(request.EmployerRequestId);
-            if(employerRequest != null && employerRequest.RequestStatus == Domain.Models.Enums.RequestStatus.Active)
+            if(employerRequest != null && employerRequest.RequestStatus == RequestStatus.Active)
             {
                 foreach (var response in employerRequest.ProviderResponseEmployerRequests)
                 {
-                    response.AcknowledgedAt = DateTime.UtcNow;
+                    response.AcknowledgedAt = _dateTimeProvider.Now;
                     response.AcknowledgedBy = request.AcknowledgedBy;
                 }
 
---
+++ src/SFA.DAS.RequestApprenticeTraining.Application/Commands/CancelEmployerRequest/CancelEmployerRequestCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace SFA.DAS.RequestApprenticeTraining.Application.Commands.CancelEmployerRequest
+{
+    public class CancelEmployerRequestCommandValidator : AbstractValidator<CancelEmployerRequestCommand>
+    {
+        public CancelEmployerRequestCommandValidator()
+        {
+            RuleFor(x => x.EmployerRequestId)
+                .NotEmpty().WithMessage("Employer Request Id must not be empty.");
+
+            RuleFor(x => x.CancelledBy)
+                .NotEmpty().WithMessage("Cancelled By must not be empty.");
+        }
+    }
+}
