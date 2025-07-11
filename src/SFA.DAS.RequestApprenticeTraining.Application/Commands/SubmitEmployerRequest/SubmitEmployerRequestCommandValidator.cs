a9f1eb6eab83826c67bf5ce5d5042a0568c12713
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-07-11 14:59:41 +0100

    P2-2221-Handled sonal cloud validation

--- src/SFA.DAS.RequestApprenticeTraining.Application/Commands/SubmitEmployerRequest/SubmitEmployerRequestCommandValidator.cs
+++ src/SFA.DAS.RequestApprenticeTraining.Application/Commands/SubmitEmployerRequest/SubmitEmployerRequestCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using FluentValidation;
 
 namespace SFA.DAS.RequestApprenticeTraining.Application.Commands.SubmitEmployerRequest
@@ -20,7 +21,7 @@ namespace SFA.DAS.RequestApprenticeTraining.Application.Commands.SubmitEmployerR
                 .GreaterThan(0).WithMessage("Number Of Apprentices must be greater than zero.");
 
             RuleFor(x => x.OriginalLocation)
-                .Must(x => string.IsNullOrEmpty(x) || Regex.IsMatch(x, "^[a-zA-Z0-9\\s,\\.\\-']+$"))
+                .Must(x => string.IsNullOrEmpty(x) || Regex.IsMatch(x, "^[a-zA-Z0-9\\s,\\.\\-']+$", RegexOptions.None, TimeSpan.FromMilliseconds(100)))
                 .WithMessage("Original Location contains invalid characters.");
 
             When(x => string.IsNullOrEmpty(x.SameLocation) || x.SameLocation == "Yes", () =>
